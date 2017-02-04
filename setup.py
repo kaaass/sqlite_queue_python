665562a3274125faf2f31b32105e4f15984a13bb
KAAAsS <admin@kaaass.net>
2017-02-04 20:13:04 +0800

    添加setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sqlitequeue',
+    version='0.01',
+    packages=[''],
+    url='',
+    license='GPL v2',
+    author='KAAAsS',
+    author_email='admin@kaaass.net',
+    description=''
+)
